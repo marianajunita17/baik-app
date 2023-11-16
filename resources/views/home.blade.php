b2f87e30f6fb0feb9b609879b126e51514231f77
noventaaaa <107040216+noventaaaa@users.noreply.github.com>
2023-11-16 21:14:03 +0700

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>BAIK - Bantuan Awal Ibu Untuk Kesehatan</title>
+    <title>BAIK - Bantuan Awal Ibu Untuk Kesehatan Mental Anak</title>
     <meta name="author" content="vecuro_themes">
     <meta name="description" content="Knirpse - Kindergarten & Baby Care Template">
     <meta name="keywords" content="Knirpse - Kindergarten & Baby Care Template">
