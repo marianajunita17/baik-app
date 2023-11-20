a339e3f3f0632f6a77398805f42b996b5f0cd9ff
ketketin <114962909+ketketin@users.noreply.github.com>
2023-11-21 02:22:02 +0700

    booking

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -286,7 +286,7 @@
                         </li>
                     </ul> -->
                     <div class="form-row place-order">
-                        <button type="submit" class="vs-btn"><a href="detail-pembayaran">Konfirmasi</a></button>
+                        <a href="detail-pembayaran"><button type="submit" class="vs-btn">Konfirmasi</button></a>
                     </div>
                 </div>
             </div>
@@ -297,101 +297,6 @@
     <!--======== Checkout Section ========-->
 
     <!--======== / Checkout Section ========-->
-
-    <!--==============================
-	Footer Area
-	==============================-->
-    <footer class="footer-wrapper footer-layout2">
-        <div class="shape-before"><img src="{{ asset('assets/img/shape/footer-before.png') }}" alt="shape"></div>
-        <div class="shape-mockup movingX d-none d-hd-block" data-bottom="37%" data-left="6%"><img src="{{ asset('assets/img/icon/cloud-2.png') }}" alt="shapes"></div>
-        <div class="shape-mockup moving d-none d-lg-block" data-top="17%" data-right="7%"><img src="{{ asset('assets/img/icon/car-4.png') }}" alt="shapes"></div>
-        <div class="widget-area">
-            <div class="container">
-                <div class="row justify-content-between">
-                    <!-- About Widget-->
-                    <div class="col-xl-auto col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="widget footer-widget">
-                            <div class="vs-widget-about">
-                                <div class="about-logo">
-                                    <a href="index.html">
-                                        <img src="{{ asset('assets/img/logo-white.svg') }}" alt="Knirpse">
-                                    </a>
-                                </div>
-                                <p class="about-text">Eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum ultrice gravida isus commodo viverra.</p>
-                                <div class="multi-social">
-                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                    <a href="#"><i class="fab fa-twitter"></i></a>
-                                    <a href="#"><i class="fab fa-pinterest-p"></i></a>
-                                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Footer Widget-->
-                    <div class="col-xl-auto col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="widget footer-widget widget_nav_menu">
-                            <h4 class="widget_title">Details Info</h4>
-                            <ul class="menu">
-                                <li><a href="classes.html">Online Class</a></li>
-                                <li><a href="contact.html">Appointment</a></li>
-                                <li><a href="about-us.html">About Us</a></li>
-                                <li><a href="team.html">Our Team</a></li>
-                                <li><a href="event.html">Upcoming Events</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- Footer Widget -->
-                    <div class="col-xl-auto col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="widget footer-widget">
-                            <h4 class="widget_title">News Feed</h4>
-                            <div class="recent-post-wrap">
-                                <div class="recent-post">
-                                    <div class="media-img">
-                                        <a href="blog-details.html"><img src="{{ asset('assets/img/blog/blog-thumb-1.jpg') }}" alt="Blog Image"></a>
-                                    </div>
-                                    <div class="media-body">
-                                        <div class="recent-post-meta"><a href="blog.html"><i class="fal fa-calendar-alt"></i> 25th March, 2023</a></div>
-                                        <h4 class="post-title"><a href="blog-details.html">User’s Pepare Using Story Guide Yes</a></h4>
-                                    </div>
-                                </div>
-                                <div class="recent-post">
-                                    <div class="media-img">
-                                        <a href="blog-details.html"><img src="{{ asset('assets/img/blog/blog-thumb-2.jpg') }}" alt="Blog Image"></a>
-                                    </div>
-                                    <div class="media-body">
-                                        <div class="recent-post-meta"><a href="blog.html"><i class="fal fa-calendar-alt"></i> 25th March, 2023</a></div>
-                                        <h4 class="post-title"><a href="blog-details.html">How to prepare child for examination</a></h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Footer Widget -->
-                    <div class="col-xl-auto col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="widget footer-widget">
-                            <h3 class="widget_title">Contact Us</h3>
-                            <div class="vs-widget-contact">
-                                <p class="footer-info"><i class="fas fa-map-marker-alt"></i>256 Lonely Street Ave, Brooklyn CA, United State</p>
-                                <p class="footer-info"><i class="fas fa-envelope"></i><a href="mailto:info@Knirpseteam.com">info@Knirpseteam.com</a></p>
-                                <p class="footer-info"><i class="fas fa-phone-alt"></i><a href="tel:+25631562156236">+256 (3156) 2156 236</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="footer-copyright">
-            <div class="container">
-
-                <p class="copyright">&copy; Copyright 2023 Knirpse. All Rights Reserved by <a href="https://themeforest.net/user/vecuro_themes">Vecuro</a>.</p>
-            </div>
-        </div>
-    </footer>
-
-    <!--********************************
-			Code End  Here
-	******************************** -->
-
 
     <!-- Scroll To Top -->
     <a href="#" class="scrollToTop scroll-btn"><i class="far fa-arrow-up"></i></a>
